2fa93c67804fa78716bd1a4d81a44bd547fbc2b2
Tawantink <92839301+tawantink@users.noreply.github.com>
2024-12-24 10:28:38 +0700

    Update configCon.php

--- configCon.php
+++ configCon.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "dab_ac"; // ชื่อผู้ใช้ MySQL ของคุณ
-$password = "Gwfj[WyZW[6II9pi"; // รหัสผ่าน MySQL ของคุณ
-$dbname = "araiwa"; // ชื่อฐานข้อมูลของคุณ
+$servername = "xefi550t7t6tjn36.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "vtbutzp63stromir"; // ชื่อผู้ใช้ MySQL ของคุณ
+$password = "eacwlellzq8coyp1"; // รหัสผ่าน MySQL ของคุณ
+$dbname = "llqkum01lyw1bhs6"; // ชื่อฐานข้อมูลของคุณ
 
 // สร้างการเชื่อมต่อ
 $conn = new mysqli($servername, $username, $password, $dbname);
