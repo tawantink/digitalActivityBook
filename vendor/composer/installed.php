5b49a7a1cd400c7c65dfe7c59c30ee56bd3752cd
Tawantink <92839301+tawantink@users.noreply.github.com>
2024-12-27 21:17:20 +0700

    Create installed.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e342f484cd174ea5f6d2a42bca23f425c2e0e191',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e342f484cd174ea5f6d2a42bca23f425c2e0e191',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
